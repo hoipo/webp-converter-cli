8c3a9e1b0df905040b07c93472fd24420fceccd9
hoipo <578344975@qq.com>
2016-11-16 14:54:52 +0800

    finish the version 0.0.1

---
+++ index.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const readChunk = require('read-chunk');
+const imageType = require('image-type');
+const execFile = require('child_process').execFile;
+const cwebp = require('cwebp-bin');
+const program = require('commander');
+const chalk = require('chalk');
+var fileList = [];
+
+function list(val) {
+    return val.split(',');
+}
+
+function walk(path) {
+    var dirList = fs.readdirSync(path);
+    dirList.forEach(function(item) {
+        if (fs.statSync(path + '/' + item).isFile()) {
+            var imageTypeData = imageType(readChunk.sync(path + '/' + item, 0, 12));
+            if (imageTypeData) {
+                if (imageTypeData.ext === 'jpg' || imageTypeData.ext === 'png') {
+                    fileList.push(path + '/' + item);
+
+                }
+            }
+        }
+    });
+
+    if (program.recursive) {
+	    dirList.forEach(function(item) {
+	        if (fs.statSync(path + '/' + item).isDirectory()) {
+	            walk(path + '/' + item);
+	        }
+	    });
+    }
+}
+
+program
+    .version('0.0.1')
+    .option('-r, --recursive', 'Walk given directory recursively')
+    .option('-f, --files <items>', 'the files you want to convert,split by \',\'', list)
+    .parse(process.argv);
+
+if (program.files) {
+    fileList = program.files;
+} else {
+    walk('./');
+}
+
+console.log(chalk.yellow("Found " + fileList.length + " image file(s) !"));
+
+fileList.forEach(function (file) {
+	execFile(cwebp, [file, '-o', file], function (err) {
+	    if (err) {
+	        throw err;
+	    }
+	    console.log(chalk.green(file + ' is converted!'));
+	});
+})
